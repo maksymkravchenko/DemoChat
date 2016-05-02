b3fc78bc732922ed5adc1b1fe9b7e18fd560ce0a
max <maximus.kravchenko@gmail.com>
2016-05-02 10:27:18 +0300

    Initial Commit

---
+++ DemoChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoChat
+//
+//  Created by Maksym Kravchenko on 5/2/16.
+//  Copyright © 2016 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
